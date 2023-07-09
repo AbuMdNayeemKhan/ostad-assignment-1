71eff9993f135255e0fbd5d8b17ebbf122b568f8
unknown <abumohammadnayemkhan@gmail.com>
2023-07-10 01:06:21 +0600

    Ostad Assignment one

---
+++ dataTypes.js
@@ -0,0 +1,61 @@
+//==========Assignment: Data Type Practice==========//
+
+// 1. Declare a variable called "name" and assign your name to it (as a string).
+
+let name = "Abu Md Nayeem Khan";
+
+
+// 2. Declare a variable called "age" and assign your age to it (as a number).
+let age = 25;
+
+
+// 3. Declare a variable called "isStudent" and assign a boolean value (true or false) indicating if you are a student or not.
+let isStudent = true;
+
+// 4. Declare a variable called "hobbies" and assign an array containing at least three of your hobbies.
+let hobbies = ["Coding", " Eating", " Cycling"];
+
+// 5. Declare a variable called "address" and assign an object with properties "street", "city", and "country" representing your address.
+let address = {
+    street : "F3",
+    city : "Dhaka",
+    country : "Bangladesh"
+}
+
+// 6. Print the following information using console.log():
+
+
+//    - Your name: "My name is [name]."
+console.log("My name is "+name+".");
+
+//    - Your age: "I am [age] years old."
+console.log("I am "+ age + " years old.");
+
+//    - Whether you are a student or not: "I am a student: [isStudent]."
+console.log("I am a student: "+isStudent+".")
+
+//    - Your hobbies: "My hobbies are [hobbies]."
+console.log("My hobbies are "+hobbies+".");
+
+//    - Your address: "I live at [street], [city], [country]."
+console.log("I live at "+ address.street +", "+ address.city +", "+ address.country +".");
+
+
+
+
+// Example Output:
+
+
+// My name is John.
+
+
+// I am 25 years old.
+
+
+// I am a student: true.
+
+
+// My hobbies are reading, painting, and hiking.
+
+
+// I live at 123 Main Street, Cityville, Countryland.
---
+++ operators.js
@@ -0,0 +1,39 @@
+// 1. Declare two variables called "num1" and "num2" and assign them any numeric values.
+let num1 = 22;
+let num2 = 11;
+
+
+// 2. Use the addition operator to calculate the sum of "num1" and "num2". Store the result in a variable called "sum".
+let sum = num1 + num2;
+
+// 3. Use the subtraction operator to calculate the difference between "num1" and "num2". Store the result in a variable called "difference".
+let difference = num1 - num2;
+
+// 4. Use the multiplication operator to calculate the product of "num1" and "num2". Store the result in a variable called "product".
+let product = num1 * num2;
+
+// 5. Use the division operator to calculate the quotient of "num1" divided by "num2". Store the result in a variable called "quotient".
+let quotient = num1 / num2;
+
+// 6. Use the modulus operator to calculate the remainder when "num1" is divided by "num2". Store the result in a variable called "remainder".
+let remainder = num1 % num2;
+
+// 7. Print the following information using console.log():
+
+
+//    - The sum of num1 and num2: "The sum is [sum]."
+console.log("The sum is " + sum + ".");
+
+
+//    - The difference between num1 and num2: "The difference is [difference]."
+console.log("The difference is " + difference + ".");
+
+//    - The product of num1 and num2: "The product is [product]."
+console.log("The product is "+product+".")
+
+
+//    - The quotient of num1 divided by num2: "The quotient is [quotient]."
+console.log("The quotient is "+ quotient +".");
+
+//    - The remainder when num1 is divided by num2: "The remainder is [remainder]."
+console.log("The quotient is "+ remainder +".");
