e1f47c03fac2c78d24c3695fe6227821a2709e05
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2025-03-02 01:02:57 +0600

    rsponsive start

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,10 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+// js for nav
+const navbtn = document.querySelector('.togoller');
+const navbar = document.querySelector('nav');
+navbtn.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+// js for from
+document.getElementById("contactForm").addEventListener("click", () => {
     alert("Thank you for your message! I will get back to you soon.");
 });
